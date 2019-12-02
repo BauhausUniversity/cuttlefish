f2944468ec946b784e89b5296f92c2993b4d7f31
mxa <max.neupert@gmx.net>
2019-12-02 12:34:47 +0100

    initial commit find word python tool

---
+++ find_word.py
@@ -0,0 +1,54 @@
+# python script to search for a string in a subtitle file
+# and output the in and out times of the found occurrances
+# write a XML snippet to be pasted into a shotcut .mlt
+
+import pandas as pd
+import srt
+import datetime
+import sys
+import getopt
+import re
+
+def main(argv):
+    inputfile = ''
+    outputfile = ''
+    word = ''
+    try:
+       opts, args = getopt.getopt(argv,"hi:o:w:",["ifile=","ofile=","word="])
+    except getopt.GetoptError:
+      print ('test.py -i <inputfile> -o <outputfile> -w <word>')
+      sys.exit(2)
+    for opt, arg in opts:
+       if opt == '-h':
+          print ('test.py -i <inputfile> -o <outputfile> -w <word>')
+          sys.exit()
+       elif opt in ("-i", "--ifile"):
+          inputfile = arg
+       elif opt in ("-o", "--ofile"):
+          outputfile = arg
+       elif opt in ("-w", "--word"):
+          word = arg
+    print ('Reading subtitle file ', inputfile)
+    print ('Output file is ', outputfile)
+    print ('Word is ', word)
+    
+    subtitle = open(inputfile, "r")
+    data = list(srt.parse(subtitle))
+ 
+    cut_list = pd.DataFrame(columns=['start', 'end', 'contant'])
+    xml = open(outputfile, "w") 
+    for i in range(len(data)):
+        if word in data[i].content:
+            start = srt.timedelta_to_srt_timestamp(data[i].start)
+            end = srt.timedelta_to_srt_timestamp(data[i].end)
+            print(start, end)
+            start = re.sub(',', '.', start)
+            end = re.sub(',', '.', end)
+            xml.write('''<entry producer="producer0" in="%s", out="%s" />\n''' %(start, end))
+            cut_list = cut_list.append({'start': start, 'end':end, 
+                                    'contant': data[i].content},
+                                   ignore_index=True)
+    cut_list
+    
+if __name__ == "__main__":
+    main(sys.argv[1:])
